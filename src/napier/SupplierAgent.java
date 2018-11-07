b6c1e239d859c15779902bfa646b8997a2766e2c
Jostein Dyrseth <josdyr@gmail.com>
2018-11-07 23:45:50 +0000

    added ECommerceOntology

--- src/napier/SupplierAgent.java
+++ src/napier/SupplierAgent.java
@@ -1,10 +1,22 @@
 package napier;
 
+import jade.content.lang.Codec;
+import jade.content.lang.sl.SLCodec;
+import jade.content.onto.Ontology;
 import jade.core.Agent;
+import supply_chain_simulation_ontology.ECommerceOntology;
 
 public class SupplierAgent extends Agent {
+	
+	private Codec codec = new SLCodec();
+	private Ontology ontology = ECommerceOntology.getInstance();
+	
 	//This method is called when the agent is launched
 	protected void setup() {
+		
+		getContentManager().registerLanguage(codec);
+		getContentManager().registerOntology(ontology);
+		
 		// Print out a welcome message
 		System.out.println("Enrolled: " + getAID().getName() + ", standing by...");
 	}
